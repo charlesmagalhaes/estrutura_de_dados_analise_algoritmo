dfe98ce15419c385ac72e235f6f1da9c91fe827a
Charles Magalhaes <charlesrm29@gmail.com>
2022-08-23 22:45:12 -0300

    Implementando o fechando do função Scanner

--- src/main/java/ExercicioQuatro.java
+++ src/main/java/ExercicioQuatro.java
@@ -35,6 +35,8 @@ public class ExercicioQuatro {
 		System.out.println("------------------------------------------------------");
 		
 		System.out.print("Pessoa(s) que roubaram/roubou: "+pessoaBandida);
+		
+		ler.close();
 
 	}
 
