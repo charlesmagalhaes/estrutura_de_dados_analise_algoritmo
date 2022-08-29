ab04d9ee922c2df8dbffc3c4594abd6f952a099c
Charles Magalhaes <charlesrm29@gmail.com>
2022-08-29 10:43:56 -0300

    Versão 2 do exercicio 2

--- src/main/java/ExercicioDois.java
+++ src/main/java/ExercicioDois.java
@@ -1,25 +1,37 @@
+import java.util.Scanner;
 
 public class ExercicioDois {
 
-	public static void main(String[] args) {
-		int[] numerosInteiros = {11, 8, 13, 19, 22, 25, 29, 33, 15, 44};
-		
-		int contador =0;
-		for (int i =0; i <  numerosInteiros.length; i++) {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int[] vetorA = new int[10];
+        
+        for (int i=0; i<vetorA.length; i++){
+            System.out.println("Entre com o valor da posição" + i);
+            vetorA[i] = scan.nextInt();
+        }
+ 
+        System.out.print("Vetor A = ");
+        for (int i=0; i<vetorA.length; i++){
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+        
+        int contador = 0;
+		for (int i =0; i <  vetorA.length; i++) {
 			contador=0;
-			for(int j = 0; j < numerosInteiros[i];j++) {
-				if(numerosInteiros[i]%(j+1)==0) {
+			for(int j = 0; j < vetorA[i];j++) {
+				if(vetorA[i]%(j+1)==0) {
 					contador++;
 				}
 			}
 			
 			if(contador==2) {
-				System.out.println("Numero primo: "+numerosInteiros[i]+" posição: "+i);
+				System.out.println("Numero primo: "+vetorA[i]+" posição: "+i);
 			}
-			
+
 		}
-	
-
-	}
-
+    }
 }
